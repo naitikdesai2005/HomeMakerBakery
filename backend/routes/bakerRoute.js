f4bf0f4da519690a0c71488d6245388d23423adf
Naitik Desai <naitikdesai3@gmail.com>
2024-07-22 14:51:50 +0530

    Update bakerRoute.js

--- backend/routes/bakerRoute.js
+++ backend/routes/bakerRoute.js
@@ -4,6 +4,6 @@ import { loginUser,registerBaker } from "../controllers/userController.js"
 const bakerRouter = express.Router()
 
 bakerRouter.post("/registerBaker",registerBaker)
-bakerRouter.post("/login",loginUser)
+// bakerRouter.post("/login",loginUser)
 
 export default bakerRouter;
