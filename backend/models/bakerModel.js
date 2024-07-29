93961d929ace252da13623df2e196d3def294874
Naitik Desai <naitikdesai3@gmail.com>
2024-07-29 15:45:25 +0530

    Update bakerModel.js

--- backend/models/bakerModel.js
+++ backend/models/bakerModel.js
@@ -1,17 +1,5 @@
 import mongoose from "mongoose";
 const bakerSchema = new mongoose.Schema({
-<<<<<<< HEAD
-    name: { type: String, required: true },
-    bakeryname: { type: String, required: true },
-    bakeryaddress: { type: String, required: true },
-    email: { type: String, required: true, unique: true },
-    password: { type: String, required: true },
-    mobilenumber: { type: Number, required: true },
-    bankAccNumber: { type: Number, required: true },
-    role: { type: String, default: "baker" },
-    products: { type: Object, default: [] },
-    select: { type: Boolean, default: true },
-=======
     name:{type:String,required:true},
     bakeryname:{type:String,required:true},
     bakeryaddress:{type:String,required:true},
@@ -22,9 +10,8 @@ const bakerSchema = new mongoose.Schema({
     role:{type:String,default:"baker"},
     products: [{ type: mongoose.Schema.Types.ObjectId, ref: 'product' }],
     select:{type:Boolean,default:true},
->>>>>>> 8741a2e56f8b78386cc46a51a92b937584d7e7f2
 
-}, { minimize: false })
+},{minimize:false})
 
-const bakerModel = mongoose.models.baker || mongoose.model("baker", bakerSchema);
+const bakerModel = mongoose.models.baker || mongoose.model("baker",bakerSchema);
 export default bakerModel;
