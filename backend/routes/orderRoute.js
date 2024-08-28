517d8cd081be8c60d2762446c09d4c086ab05ebe
Naitik Desai <naitikdesai3@gmail.com>
2024-08-28 12:46:43 +0530

    Update orderRoute.js

--- backend/routes/orderRoute.js
+++ backend/routes/orderRoute.js
@@ -1,12 +1,12 @@
 import express from "express";
-import { createOrder ,userOrders} from "../controllers/orderController.js";
-import { createOrder, updateOrderStatus } from "../controllers/orderController.js";
+import { createOrder , updateOrderStatus} from "../controllers/orderController.js";
+// import { createOrder,  } from "../controllers/orderController.js";
 import authMiddleware from "../middleware/auth.js";
 
 const orderRouter = express.Router();
 
 orderRouter.post("/create",authMiddleware,createOrder);
-orderRouter.post("/userorders",authMiddleware,userOrders);
+// orderRouter.post("/userorders",authMiddleware,userOrders);
 orderRouter.post("/update-status", authMiddleware, updateOrderStatus);
 
 export default orderRouter;
