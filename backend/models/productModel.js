d991c37cf022e92496e93c4bdf444b00de9b3a98
Naitik Desai <naitikdesai3@gmail.com>
2024-09-09 15:04:23 +0530

    latest

--- backend/models/productModel.js
+++ backend/models/productModel.js
@@ -3,7 +3,7 @@ const productSchema = new mongoose.Schema({
     name:{type:String,required:true},
     description:{type:String,required:true},
     bakerid:{type:Object,required:true},
-    bakeryName:{type:String,required:true},
+    bakeryName:{type:String},
     price:{type:Number,required:true},
     image:{type:String},
     category:{type:String,required:true},
