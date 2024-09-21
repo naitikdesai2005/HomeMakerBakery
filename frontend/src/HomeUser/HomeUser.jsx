fec0aca42273d3f7a207b1d8147bf8e1581f60f9
RoshaniDholariya <roshnidholariya@gmail.com>
2024-09-21 09:41:43 +0530

    userview

--- frontend/src/HomeUser/HomeUser.jsx
+++ frontend/src/HomeUser/HomeUser.jsx
@@ -28,10 +28,10 @@ const HomeUser = () => {
           </div>
           <img src="/images/home.jpg" alt="background" />
         </div>
-        <Menu category={category} setCategory={setCategory} />\
-        <a className="viewmore" href="/allproduct">
+        <Menu category={category} setCategory={setCategory} />
+        {/* <a className="viewmore" href="/allproduct">
           view more »»
-        </a>
+        </a> */}
         <ProductDisplay category={category} />
         <Footer />
       </div>
